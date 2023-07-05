882d2fe4fd2c5fe3613aa48c8885c2e11613887d
Ariful Islam <ariful.rony10@gmail.com>
2023-07-05 16:23:41 +0600

    grid updated

--- scripts/heroSlider.js
+++ scripts/heroSlider.js
@@ -1,5 +1,5 @@
 
-const swiper = new Swiper(".mySwiper", {
+const swiper = new Swiper(".heroSwiper", {
     initialSlide: 1, // initial number
     lazy: true,
     navigation: {
@@ -17,7 +17,7 @@ const swiper = new Swiper(".mySwiper", {
             spaceBetween: 10,
         },
         768: {
-            slidesPerView: 3,
+            slidesPerView: 2,
             spaceBetween: 40,
         },
         1024: {
@@ -39,4 +39,42 @@ const swiper = new Swiper(".mySwiper", {
 
 
 
-  // console.log(swiper.activeIndex)
+
+/* blog swipper */
+
+// blogSwiper
+
+const blogSwiper = new Swiper(".blogSwiper", {
+    initialSlide: 1, // initial number
+    lazy: true,
+    // responsive
+    // breakpoints: {
+    //     320: {
+    //         slidesPerView: 1,
+
+    //     },
+    //     640: {
+    //         slidesPerView: 2,
+    //         spaceBetween: 10,
+    //     },
+    //     768: {
+    //         slidesPerView: 3,
+    //         spaceBetween: 40,
+    //     },
+    //     1024: {
+    //         slidesPerView: 3,
+    //         spaceBetween: 80,
+    //     },
+    // },
+    zoom: true,
+    // autoplay: true, // autoplay
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 1,
+    spaceBetween: 30,
+    // loop: true,  // infinity loop
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+});
